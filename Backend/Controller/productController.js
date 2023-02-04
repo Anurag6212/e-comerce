02411a09fd70fb9c344e074e0ad7ff0eddca2f46
Anurag6212 <uanurag6212@gmail.com>
2023-02-04 18:57:49 +0530

    first basic api

--- Backend/Controller/productController.js
+++ Backend/Controller/productController.js
@@ -1,4 +1,5 @@
 const getAllProducts = async (req, res) => {
+  console.log("reaching here");
   return res.status(200).json({
     message: "Get all projects",
   });
--- Backend/app.js
+++ Backend/app.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const productRouter = express("./Routes/productRouter");
+const productRouter = require("./Routes/productRouter");
 
 const app = express();
 
